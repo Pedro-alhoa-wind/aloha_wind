8e0cac7b547faf0efd4fa947eeb3c9c6d3b2a3b5
Pedro Sanchez <pedrosanchezproyectofinal@gmail.com>
2022-11-21 12:45:01 +0100

    creacion varias paginas

---
+++ datepicker/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap min css -->
+    <link rel="stylesheet" href="/libls/bootstrap/css/bootstrap.min.css">
+
+    <!-- bootstrap datepicker css -->
+    <link rel="stylesheet" href="/datepicker/css/bootstrap-datepicker.min.css">
+
+    <title>Alhoa-Wind</title>
+</head>
+<body>
+    
+
+
+
+    <!-- libreria  -->
+    <script src="/libls/bootstrap/jquery/jquery-3.6.1.min.js"></script>
+    <script src="/libls/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/datepicker/js/bootstrap-datepicker.min.js"></script>
+</body>
+</html>
