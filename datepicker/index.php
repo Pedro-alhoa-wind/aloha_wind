347dfa8679d5750642be26ea20baa599a61fd8ff
Pedro Sanchez <pedrosanchezproyectofinal@gmail.com>
2022-11-22 13:44:39 +0100

    Estilo pagina inicial sesion

--- datepicker/index.php
+++ datepicker/index.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- libreria jquery -->
+    <script src="/libls/bootstrap/jquery/jquery-3.6.1.min.js"></script>
+
+    <!-- js para personalizarlo -->
+    <script src="/datepicker/js/app.js"></script>
+
     <!-- bootstrap min css -->
     <link rel="stylesheet" href="/libls/bootstrap/css/bootstrap.min.css">
 
@@ -20,7 +26,10 @@
 
     <!-- libreria  -->
     <script src="/libls/bootstrap/jquery/jquery-3.6.1.min.js"></script>
-    <script src="/libls/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="/libls/bootstrap/js/bootstrap.min.js"></script>
+
     <script src="/datepicker/js/bootstrap-datepicker.min.js"></script>
+    <script src="/datepicker/locales/bootstrap-datepicker.es.min.js"></script>
 </body>
 </html>
