430a57578b9d255dafcb2965d56a4417e4719446
Pedro Sanchez <pedrosanchezproyectofinal@gmail.com>
2022-11-29 21:01:55 +0100

    creado el registro campos formulario

--- login/registro.php
+++ login/registro.php
@@ -1,4 +1,5 @@
 <?php
+include_once('db.php');
 //Recibimos los datos del formulario
 $nombre=$_POST['nombre'];
 $apellido=$_POST['apellido'];
@@ -9,5 +10,9 @@ $usuario=$_POST['usuario'];
 $password=$_POST['password'];
 
 
+    $conexion=crearConexion();
+    $sql="INSER INTO usuario (nombre, apellido, fecha_nacimiento, nacionalidad, telefono, usuario, password) 
+    VALUE ('$nombre', '$apellido', '$fecha_nacimiento', '$nacionalidad', '$telefono', '$usuario', '$password')";
+    $resul = mysqli_query($conexion, $sql);
 
 ?>
