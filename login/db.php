19c82aaf3ef4528717ad70be66465462862cdf09
Pedro Sanchez <pedrosanchezproyectofinal@gmail.com>
2022-11-29 20:17:57 +0100

    creado conexion.php

--- login/db.php
+++ login/db.php
@@ -1,5 +1,30 @@
 <?php
 
-$conexion=mysqli_connect("127.0.0.1","root","","bd_alohawind");
+// Configuración necesaria para acceder a la base de datos
+function crearConexion(){
+    $hostname = "localhost";
+    $usuariodb = "root";
+    $passworddb = "";
+    $dbname = "bd_alhoawind";
+
+    //Generando la conexion con el servidor
+
+    $conexion = mysqli_connect($hostname, $usuariodb, $passworddb, $dbname);
+
+    // Si hay un error en la conexión, lo mostramos y detenemos
+
+    if (!$conexion){
+        die( "<br> Error de conexión con la base de datos: " . mysqli_connect_error());
+    }
+    else{
+        echo "Conexión exitosa";
+    }
+    return $conexion;
+
+}
+
+function cerrarConexion($conexion){
+    mysqli_close($conexion);
+}
 
 ?>
