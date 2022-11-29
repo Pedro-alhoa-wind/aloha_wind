843999f95e9a09bd93e5a6ba8646f0d3c0d29197
Pedro Sanchez <pedrosanchezproyectofinal@gmail.com>
2022-11-29 20:27:07 +0100

    creado las variables registro

---
+++ login/registro.php
@@ -0,0 +1,13 @@
+<?php
+//Recibimos los datos del formulario
+$nombre=$_POST['nombre'];
+$apellido=$_POST['apellido'];
+$fecha_nacimiento=$_POST['fecha_nacimiento'];
+$nacionalidad=$_POST['nacionalidad'];
+$telefono=$_POST['telefono'];
+$usuario=$_POST['usuario'];
+$password=$_POST['password'];
+
+
+
+?>
