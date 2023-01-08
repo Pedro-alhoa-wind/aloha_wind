fd9efad1e92f6306c1efbc1d1f42a8636a002a70
Pedro Sanchez <pedrosanchezproyectofinal@gmail.com>
2023-01-08 18:14:24 +0100

    Incluido la libreria datatables

--- pagina_admin/index.php
+++ pagina_admin/index.php
@@ -1,7 +1,7 @@
 <?php require_once "/wamp64/www/aloha_wind/pagina_admin/vistas/vista_superior.php"?>
 
 <div class="container">
-<h1>contenido principal</h1>
+    <h1>contenido principal</h1>
 </div>
 
 
