2ec55f2680a992a4dc9fb925acb3ff9dc32ab729
Pedro Sanchez <pedrosanchezproyectofinal@gmail.com>
2023-01-17 10:17:26 +0100

    MODIFICACIONES META

--- pagina_admin/crud/crud_neopreno/index.php
+++ pagina_admin/crud/crud_neopreno/index.php
@@ -12,7 +12,7 @@ $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
+    <!-- META -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="#" />  
