2283e22695043a073fa9e414f486272371bbb3ab
Pedro-alhoa-wind <pedrosanchezproyectofinal@gmail.com>
2023-02-09 01:18:07 +0100

    CREADA PAGINA INDEX

--- pagina_admin/index_reservas_clases_surf.php
+++ pagina_admin/index_reservas_clases_surf.php
@@ -236,83 +236,10 @@
 <!-- INICIO CRUD -->
 <div class="container">
 
-    <h1>TIPO DE CLASES</h1>
 
-<?php
-include_once '/wamp64/www/aloha_wind/pagina_admin/bd/bd_clases/conexion.php';
-$objeto = new Conexion();
-$conexion = $objeto->Conectar();
+    <h1>RESERVAS CLASES SURF</h1>
 
-$consulta = "SELECT id, tipo FROM tipo_clases";
-$resultado = $conexion->prepare($consulta);
-$resultado->execute();
-$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
-?>
-    
-      
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">            
-                    <button id="btnNuevo" type="button" class="btn btn-success" data-toggle="modal">Nuevo</button>    
-                    </div>    
-                </div>    
-            </div>    
-            <br>  
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="table-responsive">        
-                                <table id="tablaClases" class="table table-striped table-bordered table-condensed" style="width:100%">
-                                <thead class="text-center">
-                                    <tr>
-                                        <th>Id</th>
-                                        <th>Tipo</th>  
-                                        <th>Acciones</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php                            
-                                    foreach($data as $dat) {                                                        
-                                    ?>
-                                    <tr>
-                                        <td><?php echo $dat['id']?></td>
-                                        <td><?php echo $dat['tipo']?></td>    
-                                        <td></td>
-                                    </tr>
-                                    <?php
-                                        }
-                                    ?>                                
-                                </tbody>        
-                            </table>                    
-                            </div>
-                        </div>
-                    </div>  
-                </div>    
-      
-            <!--Modal para CRUD-->
-            <div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel"></h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                    <form id="formClases">    
-                        <div class="modal-body">
-                            <div class="form-group">
-                            <label for="tipo" class="col-form-label">Tipo:</label>
-                            <input type="text" class="form-control" id="tipo">
-                            </div>            
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
-                            <button type="submit" id="btnGuardar" class="btn btn-dark">Guardar</button>
-                        </div>
-                    </form>    
-                    </div>
-                </div>
-            </div>  
+
 </div>
 
 <!-- FIN CRUD -->
@@ -392,8 +319,7 @@ $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
     <script src="vendor/datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
     <script src="vendor/datatables/Buttons-2.3.3/js/buttons.html5.min.js"></script>
      
-    <!-- Script main -->
-    <script type="text/javascript" src="/pagina_admin/main/main_clases.js"></script>
+   
 
 </body>
 
